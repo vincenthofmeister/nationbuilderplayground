24e5ee2cff4661255a9d10c87f5c37567dd7b313
Vincent Hofmeister <vincentallanhofmeister@gmail.com>
2015-03-20 18:58:35 -0400

    environment changes

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,8 @@ module Nbplayground
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    #compile fonts
+    config.assets.paths << "#{Rails}/vendor/assets/fonts"
+
   end
 end
