b8f2c530e0200caea85bf89a09367769329d4b15
Vincent Hofmeister <vincentallanhofmeister@gmail.com>
2015-03-20 03:58:39 -0400

    remove comment

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -11,13 +11,11 @@ class ContactsController < ApplicationController
   def new_contact_point
     #this method receives payload from nationbuilder when contact is made with a person (from webhook)
 
-    #request.format = :json;
-
     contact = Contacts.create(payload: request.raw_post);
 
     contact.save;
 
-     
+
 
   end
 
