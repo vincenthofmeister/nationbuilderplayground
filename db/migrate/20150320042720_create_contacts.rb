21681ecefa9f9371e64572bf72be8313bf6f94d1
Vincent Hofmeister <vincentallanhofmeister@gmail.com>
2015-03-20 14:30:46 -0400

    change migration to use text

--- db/migrate/20150320042720_create_contacts.rb
+++ db/migrate/20150320042720_create_contacts.rb
@@ -1,7 +1,7 @@
 class CreateContacts < ActiveRecord::Migration
   def change
     create_table :contacts do |t|
-      t.string :payload
+      t.text :payload
       t.datetime :created_at
 
       t.timestamps
